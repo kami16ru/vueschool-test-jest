3cb18fbf03a48074dc390a2fb0f2c273343e4a71
Kamil Khismatullin <kami16ru@gmail.com>
2021-04-27 09:04:34 +0300

    prevent collection dataset from mutation

--- model.spec.js
+++ model.spec.js
@@ -38,3 +38,28 @@ describe('record', () => {
         spy.mockRestore()
     })
 })
+
+describe('all', () => {
+    test('returns empty model', () => {
+        const model = new Model()
+
+        expect(model.all()).toEqual([])
+    })
+
+    test('returns model data', () => {
+        const model = new Model([{
+            name: 'Batman'}, {
+            name: 'Joker'
+        }])
+
+        expect(model.all().length).toBe(2)
+    })
+
+    test('original data stays intact', () => {
+        const model = new Model([{ name: 'Batman' }])
+        const data = model.all()
+
+        data[0].name = 'Joker'
+        expect(model.$collection[0].name).toBe('Batman')
+    })
+})
